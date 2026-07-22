5fff24d1db015e9c01b179c02048998b72248218
FatimaMuhammad1 <i.fatima.is.here@gmail.com>
2026-07-22 19:36:35 +0500

    ..

--- backend/alembic/versions/add_payment_status_to_repairs.py
+++ backend/alembic/versions/add_payment_status_to_repairs.py
@@ -1,7 +1,7 @@
 """add payment status to repairs
 
 Revision ID: add_payment_status_to_repairs
-Revises: add_deposit_paid_to_repairs
+Revises: add_business_owner_role
 Create Date: 2026-07-22
 
 """
@@ -11,7 +11,7 @@ from sqlalchemy.dialects.postgresql import ENUM
 
 # revision identifiers, used by Alembic.
 revision = 'add_payment_status_to_repairs'
-down_revision = 'add_deposit_paid_to_repairs'
+down_revision = 'add_business_owner_role'
 branch_labels = None
 depends_on = None
 
